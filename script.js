c69953571d4a7657cb03c0ba4660bb4f7deabc51
Asif-vai <mr3366533@gmail.com>
2025-08-23 16:29:54 +0600

    convert number and pin to intiger

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ let loginBtn = document.getElementById("login-btn");
 loginBtn.addEventListener("click", () => {
   const phone = 174524;
   const pin = 112233;
-  let phoneNumberInput = document.getElementById("phone-number").value;
-  let pinNumberInput = document.getElementById("pin-number").value;
+  let phoneNumberInput = parseInt(document.getElementById("phone-number").value);
+  let pinNumberInput = parseInt(document.getElementById("pin-number").value);
   if (phoneNumberInput == phone && pinNumberInput == pin) {
     
     window.location.href='Home.html'
