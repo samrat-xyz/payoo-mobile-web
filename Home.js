4695d1491b7b41182e5d7e09a122dc2bd7ac8c1c
Asif-vai <mr3366533@gmail.com>
2025-08-23 19:27:09 +0600

    Added Add Money Functionality

--- Home.js
+++ Home.js
@@ -1,12 +1,24 @@
+// logout functionality 
+
+
+
+
 // Add Money Card
-document.getElementById('add-money').addEventListener('click',()=>{ 
-    document.getElementById('add-money-form').style.display = 'block';
-    document.getElementById('cashout-money-form').style.display = 'none';
-})
+
+document.getElementById("add-money").addEventListener("click", () => {
+  document.getElementById("add-money-form").style.display = "block";
+  document.getElementById("cashout-money-form").style.display = "none";
+});
+// Add Money Button
+document.getElementById("add-money-btn").addEventListener("click", () => {
+    let currentMoney = parseInt(document.getElementById("current-money").innerText)
+    let takeAmount = parseInt(document.getElementById('take-amount').value);
+    let totallMoney = currentMoney + takeAmount;
+    document.getElementById('current-money').innerText = totallMoney;
+});
 
 // Cashout Card
-document.getElementById('cashout-money').addEventListener('click',()=>{
-    document.getElementById('cashout-money-form').style.display = 'block';
-    document.getElementById('add-money-form').style.display = 'none';
-})
-
+document.getElementById("cashout-money").addEventListener("click", () => {
+  document.getElementById("cashout-money-form").style.display = "block";
+  document.getElementById("add-money-form").style.display = "none";
+});
