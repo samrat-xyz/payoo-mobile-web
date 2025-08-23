65b47abe45eef46a08a92f0cf347753ba5dd3412
Asif-vai <mr3366533@gmail.com>
2025-08-23 20:01:55 +0600

    set conditions for send money and cashout

--- Home.js
+++ Home.js
@@ -1,9 +1,9 @@
 // logout functionality 
-
+const pin = 112233;
+const Money = 45000;
 document.getElementById('logout-btn').addEventListener('click',()=>{
     window.location.href = 'index.html';
 })
-
 
 // Add Money Card
 
@@ -16,11 +16,30 @@ document.getElementById("add-money-btn").addEventListener("click", () => {
     let currentMoney = parseInt(document.getElementById("current-money").innerText)
     let takeAmount = parseInt(document.getElementById('take-amount').value);
     let totallMoney = currentMoney + takeAmount;
-    document.getElementById('current-money').innerText = totallMoney;
+    let sendMoneyPin  = parseInt(document.getElementById('sendMoney-pin').value)
+    if(sendMoneyPin == pin){
+        document.getElementById('current-money').innerText = totallMoney;
+    }else{
+        alert('Something Wrong')
+    }
+    
 });
 
 // Cashout Card
 document.getElementById("cashout-money").addEventListener("click", () => {
   document.getElementById("cashout-money-form").style.display = "block";
   document.getElementById("add-money-form").style.display = "none";
+});
+
+document.getElementById("cashout-money-btn").addEventListener("click", () => {
+    let currentMoney = parseInt(document.getElementById("current-money").innerText)
+    let cashoutAmount = parseInt(document.getElementById('cashout-amount').value);
+    let totallMoney = currentMoney - cashoutAmount;
+    let cashoutPin = parseInt(document.getElementById('cashout-pin').value);
+    if(cashoutPin == pin && cashoutAmount < Money){
+        
+        document.getElementById('current-money').innerText = totallMoney;
+    }else{
+        alert('Something Wrong')
+    }
 });
